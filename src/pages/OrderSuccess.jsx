d7b05c547446142769133633c8e6425a6c8ee9ec
destroyer19-ops <agamahalvin@gmail.com>
2026-02-27 18:43:15 +0100

    fixed Order Succcess issue

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { supabase } from "../lib/supabase";
 
 const OrderSuccess = () => {
-  return <div>OrderSuccess</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const reference = searchParams.get("reference");
+  const [order, setOrder] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("orders")
+          .select("*, packages(name, description, price)")
+          .eq("ticket_code", reference)
+          .single();
+        if (error) throw new Error(error.message);
+
+        setOrder(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrder();
+  }, [reference]);
+  console.log(order);
+  return (
+    <>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div>
+          <p>{order?.ticket_code}</p>
+          <p>{order?.student_name}</p>
+          <p>{order?.packages?.name}</p>
+          <p>{order?.pickup_day}</p>
+          <p>Succcess</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default OrderSuccess;
