9bae29ee04a5da3f7229d87b991e1aaa5b6491cd
destroyer19-ops <agamahalvin@gmail.com>
2026-05-03 23:00:54 +0100

    Fixed pricing and other minor issues

--- api/create-order.js
+++ api/create-order.js
@@ -96,7 +96,7 @@ export default async function (req, res) {
       },
       body: JSON.stringify({
         tx_ref: ticketCode,
-        amount: packageData.price / 100, // Flutterwave uses naira not kobo
+        amount: packageData.price, // Flutterwave expects amount in smallest currency unit (kobo)
         currency: "NGN",
         redirect_url: "https://tradefair-nu.vercel.app/order/success",
         customer: {
